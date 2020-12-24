1f89ba1e5149417370f10c5da2dbd93047ebbeed
Tom Hunt <techtom81@gmail.com>
2020-12-24 16:50:16 +0000

    Grid and card markup and styling

---
+++ src/components/App.js
@@ -0,0 +1,48 @@
+import Card from './Card';
+
+function App() {
+    return (
+        <div className="App">
+            <div className="container">
+                <div className="card-wrapper">
+                    <Card />
+                </div>
+                <div className="card-wrapper">
+                    <Card />
+                </div>
+                <div className="card-wrapper">
+                    <Card />
+                </div>
+                <div className="card-wrapper">
+                    <Card />
+                </div>
+                <div className="card-wrapper">
+                    <Card />
+                </div>
+                <div className="card-wrapper">
+                    <Card />
+                </div>
+                <div className="card-wrapper">
+                    <Card />
+                </div>
+                <div className="card-wrapper">
+                    <Card />
+                </div>
+                <div className="card-wrapper">
+                    <Card />
+                </div>
+                <div className="card-wrapper">
+                    <Card />
+                </div>
+                <div className="card-wrapper">
+                    <Card />
+                </div>
+                <div className="card-wrapper">
+                    <Card />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,14 @@
+const Card = () => {
+    return (
+        <button id="1" data-set="a" class="card">
+            <div className="front">
+                <img src="https://i.postimg.cc/mD3X3yPs/frozen.png" alt="frozen" />
+            </div>
+            <div className="back">
+                <img src="https://i.postimg.cc/yN98nM1Z/olaf.png" alt="" />
+            </div>
+        </button>
+    );
+};
+
+export default Card;
