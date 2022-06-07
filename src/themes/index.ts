7cce539168f49cfc0f792f5e8010d24425b4da11
Tom Hunt <techtom81@gmail.com>
2022-06-07 18:09:36 +0100

    Added Hulk and Ariel themes

--- src/themes/index.ts
+++ src/themes/index.ts
@@ -2,4 +2,6 @@ export const themes = [
   { id: 0, name: 'frozen', maxCards: 12, logo: './images/themes/frozen/frozen.png' },
   { id: 1, name: 'lego', maxCards: 12, logo: './images/themes/lego/lego.png' },
   { id: 2, name: 'masha', maxCards: 12, logo: './images/themes/masha/masha.png' },
+  { id: 3, name: 'ariel', maxCards: 12, logo: './images/themes/ariel/ariel.png' },
+  { id: 4, name: 'hulk', maxCards: 12, logo: './images/themes/hulk/hulk.png' },
 ]
